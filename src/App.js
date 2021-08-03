0b9376ae0576e646036d7fd57fbe440d3d0146e2
Jason Aravanis <j.aravanis@icloud.com>
2021-08-03 21:57:06 +1000

    Install react-router, set up first route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="h-full bg-gray-50">
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/home">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
