7568c10fd55694de103fff0157c14f180dbc15fd
Jason Aravanis <j.aravanis@icloud.com>
2021-08-06 18:47:30 +1000

    Move SVGs into components

---
+++ src/images/SVG/PaperAirplane.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function PaperAirplane() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-7 w-7 m-2 text-gray-700"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1"
+        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+      />
+    </svg>
+  );
+}
+
+export default PaperAirplane;
