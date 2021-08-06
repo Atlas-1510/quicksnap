1fc482230e1149e7d05641efca3a260b72c4c3a3
Jason Aravanis <j.aravanis@icloud.com>
2021-08-06 18:52:21 +1000

    Add authentication check for home page load
    * Will load sign in page if no user found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
+  const user = true;
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            if (user) {
+              return <Home />;
+            } else {
+              return <Login />;
+            }
+          }}
+        ></Route>
       </Switch>
     </Router>
   );
