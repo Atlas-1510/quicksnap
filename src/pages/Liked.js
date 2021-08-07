879a7be27b59f06ce859aa2171bf382bf576befe
Jason Aravanis <j.aravanis@icloud.com>
2021-08-07 21:07:17 +1000

    Add placeholder pages for Liked and Explore

---
+++ src/pages/Liked.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Liked() {
+  return <div>Hello from liked</div>;
+}
+
+export default Liked;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Home from "./Home";
 import Messenger from "./Messenger";
+import Liked from "./Liked";
+import Explore from "./Explore";
 
 function Main() {
   const [currentPage, setCurrentPage] = useState("home");
@@ -13,10 +15,14 @@ function Main() {
       <div className="bg-gray-200 pt-12 flex flex-col min-h-full">
         <Switch>
           <Route exact path="/" component={Home}></Route>
-        </Switch>
-        <Switch>
           <Route exact path="/messenger">
             <Messenger />
+          </Route>
+          <Route exact path="/liked">
+            <Liked />
+          </Route>
+          <Route exact path="/explore">
+            <Explore />
           </Route>
         </Switch>
       </div>
