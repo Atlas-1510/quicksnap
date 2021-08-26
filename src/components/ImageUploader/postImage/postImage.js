0a3399c14da56682bdbd09ef2f3eede1545e21df
Jason Aravanis <j.aravanis@icloud.com>
2021-08-26 17:47:50 +1000

    Remove old console logs from postImage.js

--- src/components/ImageUploader/postImage/postImage.js
+++ src/components/ImageUploader/postImage/postImage.js
@@ -1,18 +1,13 @@
 import { storage, firestore } from "../../../firebase/firebase";
 
 export default async function postImage(user, image) {
-  console.log("USER");
-  console.log(user);
-  console.log("IMAGE");
-  console.log(image);
-
   const storageRef = storage.ref().child(`${user.uid}/posts/${image.name}`);
   await storageRef.put(image, {
     contentType: "image/jpeg",
   });
   const imageURL = await storageRef.getDownloadURL();
 
-  const postRef = await firestore.collection("posts").add({
+  await firestore.collection("posts").add({
     author: {
       name: user.name,
       profileImage: user.profileImage,
