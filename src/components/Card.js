737dd8d01dc9ac1d28dd6429c033f0918216649d
Jason Aravanis <j.aravanis@icloud.com>
2021-09-02 19:05:00 +1000

    Add layout for comment input on posts

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,13 @@ import Bookmark from "../images/SVG/Bookmark/Bookmark";
 import ThreeDots from "../images/SVG/ThreeDots";
 import { Link } from "react-router-dom";
 
+// TODO: Add timestamp display to post
+
 function Card({ card }) {
   const { author, image, comments, likeCount } = card;
 
   return (
-    <div className="flex flex-col md:border border-gray-300 rounded-sm md:mt-7 text-sm">
+    <div className="flex flex-col md:border border-gray-300 rounded-sm mb-3 md:mt-7 text-sm ">
       <div className="flex justify-between bg-white h-12 items-center">
         <div className="mx-3 flex items-center">
           <img
@@ -54,10 +56,22 @@ function Card({ card }) {
         <div className="mx-3 mt-1 mb-3">
           {comments.map((comment) => (
             <div key={comment.id}>
-              <span className="font-bold ">{comment.author}</span>
+              <Link to={`/view-user/${comment.author.id}`}>
+                <span className="font-bold ">{comment.author.name}</span>
+              </Link>
               <span> {comment.content}</span>
             </div>
           ))}
+        </div>
+        <div className="flex p-1 border-t border-gray-200">
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            className="w-full p-2  focus:outline-none focus:ring-1 focus:border-blue-300 border-0 rounded-md"
+          />
+          <button className="mx-3 text-blue-500 font-semibold cursor-pointer">
+            Post
+          </button>
         </div>
       </div>
     </div>
