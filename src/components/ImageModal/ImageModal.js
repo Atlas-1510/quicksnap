10d016540f03c879cfdf360a106abde353f67c14
Jason Aravanis <j.aravanis@icloud.com>
2021-09-05 13:20:06 +1000

    Merge mobile ImageModal into desktop component

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -4,13 +4,23 @@ import submitComment from "../../utils/submitComment/submitComment";
 import updatePost from "../../utils/updatePost/updatePost";
 
 import useWindowSize from "../../hooks/useWindowSize/useWindowSize";
-import Desktop from "./Desktop/Desktop";
-import Mobile from "./Mobile/Mobile";
 
 import useGetPostHeartStatus from "../../hooks/useGetPostHeartStatus/useGetPostHeartStatus";
 import likePost from "../../utils/likePost/likePost";
 import unlikePost from "../../utils/unlikePost/unlikePost";
 import getLikedByInfo from "../../utils/getLikedByInfo/getLikedByInfo";
+
+// ********
+
+import ModalBackground from "../ModalBackground";
+import useComponentVisible from "../../hooks/useComponentVisible/useComponentVisible";
+import ThreeDots from "../../images/SVG/ThreeDots";
+import Heart from "../../images/SVG/Heart/Heart";
+import PaperAirplane from "../../images/SVG/PaperAirplane/PaperAirplane";
+import Bookmark from "../../images/SVG/Bookmark/Bookmark";
+import ChevronLeft from "../../images/SVG/ChevronLeft";
+
+// ********
 
 function ImageModal({ post, setActivePost }) {
   const { width } = useWindowSize();
@@ -65,36 +75,166 @@ function ImageModal({ post, setActivePost }) {
     const likedByInfo = await getLikedByInfo(id);
     console.log(likedByInfo);
   };
+  const { ref, isComponentVisible } = useComponentVisible(true, exit);
 
-  if (width > 768) {
-    return (
-      <Desktop
-        post={postInfo}
-        exit={exit}
-        initCommentSubmit={initCommentSubmit}
-        setCommentInput={setCommentInput}
-        commentInput={commentInput}
-        setHandleLikeChange={setHandleLikeChange}
-        liked={liked}
-        likeCountDisplay={likeCountDisplay}
-        handleShowLikedByModal={handleShowLikedByModal}
-      />
-    );
-  } else {
-    return (
-      <Mobile
-        post={postInfo}
-        exit={exit}
-        initCommentSubmit={initCommentSubmit}
-        setCommentInput={setCommentInput}
-        commentInput={commentInput}
-        setHandleLikeChange={setHandleLikeChange}
-        liked={liked}
-        likeCountDisplay={likeCountDisplay}
-        handleShowLikedByModal={handleShowLikedByModal}
-      />
-    );
-  }
+  return (
+    <ModalBackground exit={exit}>
+      <div
+        ref={ref}
+        className={
+          width > 768
+            ? "w-2/3 max-w-4xl"
+            : "bg-white absolute top-0 w-full h-full"
+        }
+      >
+        {isComponentVisible && (
+          <>
+            {width < 768 && (
+              <div className="border-b border-gray-300 flex  items-center justify-between py-2">
+                <div className="mx-2 w-7" onClick={() => exit()}>
+                  <ChevronLeft />
+                </div>
+                <div className="mx-3 flex items-center">
+                  <img
+                    className="w-8 h-8 md:border rounded-full"
+                    src={post.author.profileImage}
+                    alt="Author profile"
+                  />
+                  <span className="mx-3">{post.author.name}</span>
+                </div>
+                <div className="w-7 m-2">
+                  <ThreeDots />
+                </div>
+              </div>
+            )}
+            <div
+              className={
+                width > 768
+                  ? "grid grid-cols-3 grid-rows-1"
+                  : "flex flex-col text-sm"
+              }
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img
+                className="col-start-1 col-span-2 min-w-full"
+                style={{ maxHeight: "80vh" }}
+                data-testid="test-image-modal-desktop"
+                src={post.image}
+                alt="Full size view of clicked thumbnail"
+              />
+
+              <div className="flex flex-col text-sm">
+                <div className="flex justify-between bg-white h-12 items-center">
+                  <div className="mx-3 flex items-center">
+                    <img
+                      className="w-8 h-8 md:border rounded-full"
+                      src={post.author.profileImage}
+                      alt="Author profile"
+                    />
+                    <span className="mx-3">{post.author.name}</span>
+                  </div>
+                  <div className="w-7 m-2">
+                    <ThreeDots />
+                  </div>
+                </div>
+                <div className="flex flex-col bg-white text-gray-700 flex-grow">
+                  <div className="flex justify-between">
+                    <div className="flex">
+                      <div
+                        className="w-8 m-2 cursor-pointer"
+                        onClick={setHandleLikeChange}
+                      >
+                        <Heart liked={liked} />
+                      </div>
+                      <div className="w-8 m-2">
+                        <PaperAirplane />
+                      </div>
+                    </div>
+                    <div className="w-8 m-2">
+                      <Bookmark />
+                    </div>
+                  </div>
+                  <div onClick={handleShowLikedByModal}>
+                    {likeCountDisplay === 1 && (
+                      <span className="mx-3 my-1">
+                        Liked by{" "}
+                        <span className="font-bold">
+                          {likeCountDisplay} user
+                        </span>
+                      </span>
+                    )}
+                    {likeCountDisplay > 1 && (
+                      <span className="mx-3 my-1">
+                        Liked by{" "}
+                        <span className="font-bold">
+                          {likeCountDisplay} users
+                        </span>
+                      </span>
+                    )}
+                  </div>
+                  <div className="mx-3 mt-1 mb-3 flex-grow">
+                    {post.comments.map((comment) => (
+                      <div key={comment.id}>
+                        <span className="font-bold">{comment.author.name}</span>
+                        <span> {comment.content}</span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex p-1 border-t border-gray-200">
+                    <input
+                      type="text"
+                      placeholder="Add a comment..."
+                      className="w-full p-2  focus:outline-none focus:ring-1 focus:border-blue-300 border-0 rounded-md"
+                      value={commentInput}
+                      onChange={(e) => {
+                        setCommentInput(e.target.value);
+                      }}
+                    />
+                    <button
+                      className="mx-3 text-blue-500 font-semibold cursor-pointer"
+                      onClick={(e) => initCommentSubmit(e)}
+                    >
+                      Post
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </ModalBackground>
+  );
+
+  // if (width > 768) {
+  //   return (
+  //     <Desktop
+  //       post={postInfo}
+  //       exit={exit}
+  //       initCommentSubmit={initCommentSubmit}
+  //       setCommentInput={setCommentInput}
+  //       commentInput={commentInput}
+  //       setHandleLikeChange={setHandleLikeChange}
+  //       liked={liked}
+  //       likeCountDisplay={likeCountDisplay}
+  //       handleShowLikedByModal={handleShowLikedByModal}
+  //     />
+  //   );
+  // } else {
+  //   return (
+  //     <Mobile
+  //       post={postInfo}
+  //       exit={exit}
+  //       initCommentSubmit={initCommentSubmit}
+  //       setCommentInput={setCommentInput}
+  //       commentInput={commentInput}
+  //       setHandleLikeChange={setHandleLikeChange}
+  //       liked={liked}
+  //       likeCountDisplay={likeCountDisplay}
+  //       handleShowLikedByModal={handleShowLikedByModal}
+  //     />
+  //   );
+  // }
 }
 
 export default ImageModal;
