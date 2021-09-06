630915495f8581e20f7c5772b11a86b0c726c1b5
Jason Aravanis <j.aravanis@icloud.com>
2021-09-06 19:05:41 +1000

    Add layout for recent searches modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,17 @@ import Home from "../../images/SVG/Home/Home";
 import PaperAirplane from "../../images/SVG/PaperAirplane/PaperAirplane";
 import Profile from "../../images/SVG/Profile/Profile";
 import Camera from "../../images/SVG/Camera";
-
 import ImageUploader from "../ImageUploader/ImageUploader";
+
+import testProfile from "../../images/test-images/testUserProfile.png";
+import Exit from "../../images/SVG/Exit";
 
 function Header({ currentPage, setCurrentPage }) {
   const [uploadModal, setUploadModal] = useState(false);
+
+  const [recentSearches, setRecentSearches] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+  ]);
 
   return (
     <>
@@ -80,8 +86,49 @@ function Header({ currentPage, setCurrentPage }) {
           />
         </ModalBackground>
       )}
+      {/* Search modal */}
+      <RecentSearchModal recentSearches={recentSearches} />
     </>
   );
 }
 
 export default Header;
+
+function RecentSearchModal({ recentSearches }) {
+  return (
+    <div className="absolute left-96 top-14 flex flex-col items-center">
+      <div className="w-4 h-4 transform rotate-45 bg-white absolute -top-2 shadow-lg z-40"></div>
+      <div className="w-96 h-96 bg-white z-50 flex flex-col items-center shadow-xl border-0 rounded-md p-3">
+        <div className="flex justify-between w-full">
+          <span className="font-semibold text-lg">Recent</span>
+          <span className="font-semibold text-sm text-blue-500 cursor-pointer">
+            Clear All
+          </span>
+        </div>
+        <div className="w-full overflow-y-scroll">
+          {/*  */}
+          {recentSearches.map((index) => {
+            return (
+              <div className="flex my-2 items-center w-full justify-between">
+                <img
+                  alt="User"
+                  src={testProfile}
+                  className="h-10 w-10 border rounded-full"
+                />
+                <div className="flex flex-col flex-grow ml-3">
+                  <span className="font-semibold text-sm">iamjasona</span>
+                  <span className="text-gray-500 text-xs">Jason Aravanis</span>
+                </div>
+                <div className="w-7 m-2">
+                  <Exit />
+                </div>
+              </div>
+            );
+          })}
+
+          {/*  */}
+        </div>
+      </div>
+    </div>
+  );
+}
