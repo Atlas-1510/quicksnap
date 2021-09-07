a0ca373cf047a00f9765452ce761775a3e4b38c3
Jason Aravanis <j.aravanis@icloud.com>
2021-09-07 13:43:45 +1000

    Add user ID to return result from getUserinfo()

--- src/utils/getUserInfo/getUserInfo.js
+++ src/utils/getUserInfo/getUserInfo.js
@@ -3,6 +3,9 @@ import { firestore } from "../../firebase/firebase";
 export default async function getUserInfo(id) {
   const userRef = firestore.collection("users").doc(id);
   const userDoc = await userRef.get();
-  const user = userDoc.data();
+  const user = {
+    ...userDoc.data(),
+    id: userDoc.id,
+  };
   return user;
 }
