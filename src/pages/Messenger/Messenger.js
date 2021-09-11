fbf66c534a27e5f907427874bc3d4909194a788d
Jason Aravanis <j.aravanis@icloud.com>
2021-09-12 09:48:08 +1000

    Update Messenger with connection to firestore
    * Can access existing chats
    * Can access messages within chat when a chat is opened

--- src/pages/Messenger/Messenger.js
+++ src/pages/Messenger/Messenger.js
@@ -1,49 +1,105 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import useWindowSize from "../../hooks/useWindowSize/useWindowSize";
-
-import getContacts from "./getContacts/getContacts";
-import getMessages from "./getMessages/getMessages";
 
 import Desktop from "./Desktop/Desktop";
 import Mobile from "./Mobile/Mobile";
+// import useGetChats from "../../hooks/useGetChats/useGetChats";
+// import useGetMessages from "../../hooks/useGetMessages/useGetMessages";
+import { UserContext } from "../Main";
+import { firestore } from "../../firebase/firebase";
 
 function Messenger({ setCurrentPage }) {
   const { width } = useWindowSize();
-  const [contacts, setContacts] = useState([]);
-  const [activeContact, setActiveContact] = useState(null);
+  const { uid } = useContext(UserContext);
+  const [chats, setChats] = useState(null);
+  useEffect(() => {
+    const unsub = firestore
+      .collection("chats")
+      .where("members", "array-contains", uid)
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snap) => {
+        let chatDocs = [];
+        snap.forEach((chat) => {
+          chatDocs.push({ ...chat.data(), chatID: chat.id });
+        });
+        chatDocs = chatDocs.map((chat) => {
+          let membersWithoutCurrentUser = chat.members.filter(
+            (id) => id !== uid
+          );
+          let resolve = {
+            ...chat,
+            contact: membersWithoutCurrentUser[0],
+            members: undefined,
+          };
+          return resolve;
+        });
+        const promises = [];
+        chatDocs.forEach((doc) => {
+          const promise = firestore.collection("users").doc(doc.contact).get();
+          promises.push(promise);
+        });
+        const users = [];
+        Promise.all(promises).then((results) => {
+          results.forEach((result) => {
+            const user = result.data();
+            users.push({ ...user, id: result.id });
+          });
+          users.forEach((user) => {
+            chatDocs.forEach((doc) => {
+              if (user.id === doc.contact) {
+                doc.contact = user;
+              }
+            });
+          });
+
+          setChats(chatDocs);
+        });
+      });
+    return () => unsub();
+  }, [uid]);
+  const [activeChat, setActiveChat] = useState(null);
   const [messages, setMessages] = useState(null);
+
+  useEffect(() => {
+    if (activeChat && activeChat !== "launchpad") {
+      console.log(`useGetChats chatID: ${activeChat}`);
+      const unsub = firestore
+        .collection("chats")
+        .doc(activeChat)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .limit(10)
+        .onSnapshot((snap) => {
+          const msgDocs = [];
+          snap.forEach((msg) => {
+            msgDocs.push({ ...msg.data(), id: msg.id });
+          });
+          setMessages(msgDocs);
+        });
+      return () => unsub();
+    } else if (activeChat === "launchpad") {
+      setMessages([]);
+    }
+  }, [activeChat]);
+
   const [newMessage, setNewMessage] = useState(false);
 
   useEffect(() => {
-    //TODO: Replace getContacts with firebase call. Remember to update tests to mock firebase instead of getContacts
-    let newContacts = getContacts();
-    setContacts(newContacts);
-  }, []);
-
-  const handleClick = (e) => {
-    const userid = e.target.dataset.userid;
-    let user = contacts.find((contact) => contact.id === userid);
-    setActiveContact(user);
-  };
+    console.log(chats);
+  }, [chats]);
 
   useEffect(() => {
-    if (activeContact) {
-      //TODO: Replace getMessages with firebase call. Remember to update tests to mock firebase instead of getMessages
-      setMessages(getMessages(activeContact));
-    }
-  }, [activeContact]);
+    console.log(messages);
+  }, [messages]);
 
   // Mobile messenger - max width of 768 based on tailwind screen sizes
   if (width < 768) {
     return (
       <Mobile
-        handleClick={handleClick}
-        contacts={contacts}
-        setContacts={setContacts}
-        activeContact={activeContact}
-        setActiveContact={setActiveContact}
+        chats={chats}
+        activeChat={activeChat}
+        setActiveChat={setActiveChat}
         messages={messages}
-        setMessages={setMessages}
         newMessage={newMessage}
         setNewMessage={setNewMessage}
         setCurrentPage={setCurrentPage}
@@ -52,13 +108,11 @@ function Messenger({ setCurrentPage }) {
   } else if (width >= 768) {
     return (
       <Desktop
-        handleClick={handleClick}
-        contacts={contacts}
-        setContacts={setContacts}
-        activeContact={activeContact}
-        setActiveContact={setActiveContact}
+        chats={chats}
+        setChats={setChats}
+        activeChat={activeChat}
+        setActiveChat={setActiveChat}
         messages={messages}
-        setMessages={setMessages}
         newMessage={newMessage}
         setNewMessage={setNewMessage}
         setCurrentPage={setCurrentPage}
