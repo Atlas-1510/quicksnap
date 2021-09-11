ff3e851be5fd30207e51e723cc94a152040fb5a2
Jason Aravanis <j.aravanis@icloud.com>
2021-09-12 09:47:09 +1000

    Update Desktop view of messenger to handle new data structure from Messenger component

--- src/pages/Messenger/Desktop/Desktop.js
+++ src/pages/Messenger/Desktop/Desktop.js
@@ -9,19 +9,15 @@ import Button from "../../../components/Button";
 import { UserContext } from "../../Main";
 
 import ChatBox from "../ChatBox/ChatBox";
-import NewMessage from "../NewMessage/NewMessage";
+import NewChat from "../NewChat/NewChat";
 
 function Desktop({
-  handleClick,
-  contacts,
-  setContacts,
-  activeContact,
-  setActiveContact,
+  chats,
+  setChats,
+  setActiveChat,
   messages,
-  setMessages,
   newMessage,
   setNewMessage,
-  setCurrentPage,
 }) {
   const user = useContext(UserContext);
   return (
@@ -39,7 +35,7 @@ function Desktop({
             </div>
           </div>
           <div
-            className=" mx-2 w-7 absolute right-0"
+            className=" mx-2 w-7 absolute right-0 cursor-pointer"
             onClick={() => setNewMessage(true)}
             data-testid="test-write-button"
           >
@@ -47,30 +43,33 @@ function Desktop({
           </div>
         </div>
         <div className="flex flex-col">
-          {contacts.map((contact) => (
-            <div
-              key={contact.id}
-              className="flex items-center m-2"
-              onClick={handleClick}
-              data-userid={contact.id}
-            >
-              <img
-                src={contact.profileImage}
-                alt="contact"
-                className="border rounded-full h-12 pointer-events-none"
-                data-testid={`user-image-${contact.id}`}
-              />
-              <div className="ml-3 flex flex-col pointer-events-none">
-                <span className="font-semibold text-sm">{contact.name}</span>
-                <span className="text-gray-500 text-xs">
-                  {contact.fullName}
-                </span>
+          {chats &&
+            chats.map((chat) => (
+              <div
+                key={chat.chatID}
+                className="flex items-center m-2 cursor-pointer"
+                onClick={() => setActiveChat(chat.chatID)}
+              >
+                <img
+                  src={chat.contact.profileImage}
+                  alt="contact"
+                  className="border rounded-full h-12 pointer-events-none"
+                  data-testid={`user-image-${chat.contact.id}`}
+                />
+                <div className="ml-3 flex flex-col pointer-events-none">
+                  <span className="font-semibold text-sm">
+                    {chat.contact.name}
+                  </span>
+                  <span className="text-gray-500 text-xs">
+                    {chat.contact.fullName}
+                  </span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
       <div className="flex col-span-2 h-full border-l border-gray-300 flex-col justify-center">
+        {" "}
         {/* Guide to send first message if no messages loaded */}
         {!messages && (
           <div className="flex flex-col justify-center items-center">
@@ -92,11 +91,11 @@ function Desktop({
         {newMessage && (
           <ModalBackground closeFunction={() => setNewMessage(false)}>
             <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-1/2 h-1/2 bg-white border rounded-md border-gray-300">
-              <NewMessage
+              <NewChat
                 exit={() => setNewMessage(false)}
-                setContacts={setContacts}
-                contacts={contacts}
-                setActiveContact={setActiveContact}
+                setActiveChat={setActiveChat}
+                chats={chats}
+                setChats={setChats}
               />
             </div>
           </ModalBackground>
