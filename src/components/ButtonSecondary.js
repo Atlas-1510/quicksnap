a6b726f8fce1240fc082a3d50f458f489e51f80c
Jason Aravanis <j.aravanis@icloud.com>
2021-09-18 23:04:16 +1000

    Add animation to secondary button

--- src/components/ButtonSecondary.js
+++ src/components/ButtonSecondary.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function ButtonSecondary({ children }) {
   return (
-    <button className="bg-gray-400 my-3 py-1 px-2 border-0 rounded-sm text-white hover:shadow-inner hover:bg-gray-300">
+    <motion.button
+      className="bg-gray-400 my-3 py-1 px-2 border-0 rounded-sm text-white hover:shadow-inner hover:bg-gray-300"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
       {children}
-    </button>
+    </motion.button>
   );
 }
 
